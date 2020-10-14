b1471fbd1333ec54d11111205dea44cd54f66729
Jorge Hernandez <jorgehdzg1991@gmail.com>
2020-10-14 14:38:10 -0500

    [UPDATE] Add last name to greet

--- Saludo.js
+++ Saludo.js
@@ -1,11 +1,13 @@
 module.exports = class Saludo {
   nombre;
+  apellido;
 
-  constructor(nombre) {
+  constructor(nombre, apellido) {
     this.nombre = nombre;
+    this.apellido = apellido
   }
   
   saluda() {
-    console.info(`Hola ${this.nombre}!`);
+    console.info(`Hola ${this.nombre} ${this.apellido}!`);
   }
 };
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var Saludo = require('./Saludo');
 var SaludoArgs = require('./SaludoArgs');
 
-var saludo = new Saludo(SaludoArgs.nombre);
+var saludo = new Saludo(SaludoArgs.nombre, SaludoArgs.apellido);
 
 saludo.saluda();
